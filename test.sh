12a83ff4067f02aabfff60bc2cd2e7dfca78cc1e
Igor Ivanov <ivanov.i@gmail.com>
2020-10-27 12:19:20 +0700

    Add context variables file

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-git ls-files | entr sh -c 'clear && cd Webhooks_build && cmake .. && cmake --build . && ./Webhooks'
+while true; do
+
+	{ git ls-files; git ls-files . --exclude-standard --others; } | entr -d sh -c 'clear && cd Webhooks_build && cmake .. && cmake --build . && ./Webhooks'
+
+done
 
--- webhooks_test.cpp
+++ webhooks_test.cpp
@@ -1,12 +1,26 @@
 #include <boost/ut.hpp>
 
+
 using namespace boost::ut;
 using namespace boost::ut::bdd;
 using namespace boost::ut::literals;
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+#include "ContextVariables.h"
 
 suite webhooks_test = []
 {
-	"empty"_test = []
+	"Array variable"_test = []
 	{
+		when("given variable name") = []
+		{
+			const auto name = L"variable"s;
+
+			then("can create variable") = []
+			{
+				using namespace ContextVariables;
+			};
+		};
 	};
 };
